56e25d93a59ca4bcf6c286debac01c70cc0aa711
longquoc47krb <54442229+longquoc47krb@users.noreply.github.com>
2021-11-14 14:30:58 +0700

    Update index.js

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,2 +1,5 @@
 module.exports.authController = require('./auth.controller');
 module.exports.userController = require('./user.controller');
+module.exports.stationController = require('./station.controller');
+module.exports.ticketController = require('./ticket.controller');
+module.exports.routeContrller = require('./route.controller');
